aae5b27fc109058657174e3e47fb3e94231d19cc
chenshun00 <13786285072@163.com>
2019-05-26 10:06:48 +0800

    httpClient

---
+++ plugins/httpClient3/src/main/java/top/huzhurong/plugin/impl/intercepter/HttpClient3Hook.java
@@ -0,0 +1,28 @@
+package top.huzhurong.plugin.impl.intercepter;
+
+import top.huzhurong.test.bootcore.BaseHook;
+import top.huzhurong.test.bootcore.BeanMethodRegister;
+
+/**
+ * @author chenshun00@gmail.com
+ * @since 2019/5/26
+ */
+public class HttpClient3Hook implements BaseHook {
+
+    public static HttpClient3Hook Instance = new HttpClient3Hook();
+
+    @Override
+    public void into(Object curObject, int index, Object[] args) {
+        System.out.println("into \t" + BeanMethodRegister.get(index).toString());
+    }
+
+    @Override
+    public void out(Object result, Object cur, int index, Object[] args) {
+        System.out.println("out\t" + BeanMethodRegister.get(index).toString());
+    }
+
+    @Override
+    public void error(Throwable ex, Object curObject, int index, Object[] args) {
+
+    }
+}
